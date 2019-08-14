179f684608e815237eee6d424fb1e26b48f95514
clank3140 <clank3140.mo.de@gmail.com>
2019-08-15 01:29:13 +0900

    popup and inserting before of  <h1>

--- content_script.js
+++ content_script.js
@@ -1,6 +1,6 @@
 $(function() {
-
+    console.log('start');
   // 自要素の前に追加
-  $('body').before('<div class="add">body要素の上に追加しました。</div>');
-
+  $('h1').before('AAAAAAAAAAAAAAAAAAAAAAAAa');
+console.log('end');
 });
