88b540b4d339bc1f3921b47fa9c7d9b77ab9425e
clank3140 <clank3140.mo.de@gmail.com>
2019-08-21 01:31:49 +0900

    fix CSS and variables

--- content_script.js
+++ content_script.js
@@ -1,16 +1,40 @@
+function get_title(links) {
+    $(function () {
+        var title;
+        $.ajax({
+            type: 'GET',
+            url: links,
+            success: function (data) {
+                title = data.match(/<title>(.*)<\/title>/);
+            }
+        })
+        console.log(links);
+        console.log(title);
+        return title[1];
+    });
+};
+
+function AddLinkButton(DOM) {
+    console.log(DOM);
+    $(DOM).each(function () {
+        $(this).addClass("link_button");
+        console.log('end');
+    })
+};
+
 $(function () {
+    var links;
+    var title;
+    var place_URL = "#description > yt-formatted-string > a";
     setTimeout(function () {
         console.log('start');
-        var links = 1;
-        var name;
-        $("#description > yt-formatted-string > a").each(function(){
-            links = $(this).attr('href');
-            name = $(this).text();
+        $(place_URL).each(function () {
             console.log(this);
-            console.log(links);
-            console.log(name);
-            $("#info").before("<a href="+links+">"+name+"</a>");
+            links = $(this).attr('href').replace('http:', 'https:');
+            title = "aaaa";
+            $("#info").before("<extension><a href=" + links + ">" + title + "</a></extension>");
         });
-        console.log('end');
-    }, 2000);
+        console.log('point');
+        AddLinkButton("extension");
+    }, 2000)
 });
