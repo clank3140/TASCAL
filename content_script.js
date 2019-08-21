fe87f876bb5854b81bdd6d347d89403682e84d4b
clank3140 <clank3140.mo.de@gmail.com>
2019-08-21 13:51:58 +0900

    @つきと＃つきのリンクはタイトルが出るように

--- content_script.js
+++ content_script.js
@@ -1,13 +1,37 @@
 function get_title(links) {
-    chrome.runtime.sendMessage(links, function (response) {
-        console.log('response : ' + response);
-        return response;
-    });
+    //    if (links == 'https://www.youtube.com/*') {
+    //        chrome.runtime.sendMessage(links, function (response) {
+    //            console.log('response : ' + response);
+    //            return response;
+    //        });
+    //    }
+
+    if (links.includes('search_query')) {
+        console.log('search! ' + links);
+        links = links.replace('/results?search_query=', '');
+        var title = decodeURI(links);
+        if (title.includes('%23')) {
+            title = title.replace('%23', '#');
+        } else {
+            title = '検索：' + title;
+        };
+        return title;
+    };
+
+    if (links.includes('twitter.com')) {
+        var id = links.replace('https://twitter.com/', '@');
+        return id;
+    };
 };
 
 function AddLinkButton(DOM) {
     $(DOM).each(function () {
         $(this).addClass("link_button");
+        if ($(this).children('a').attr('href').includes('twitter.com') || $(this).children('a').attr('href').includes('search_query')) {
+            $(this).children('a').addClass('twitter');
+        } else {
+            $(this).children('a').addClass('others');
+        };
         console.log('end');
     })
 };
@@ -20,8 +44,13 @@ $(function () {
         console.log('start');
         $(place_URL).each(function () {
             console.log(this);
-            links = $(this).text().replace('http:', 'https:');
+            if ($(this).attr('href').includes('twitter.com')) {
+                links = $(this).text();
+            } else {
+                links = $(this).attr('href');
+            };
             title = get_title(links);
+            console.log(title);
             $("#info").before("<extension><a href=" + links + ">" + title + "</a></extension>");
         });
         AddLinkButton("extension");
