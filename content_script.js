aeafb76277a4f7274e7e534637113ac63a91b810
clank3140 <clank3140.mo.de@gmail.com>
2019-08-21 14:20:04 +0900

    ハッシュタグ検索はTwitterに飛ぶように

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-function get_title(links) {
+function get_title(URL) {
     //    if (links == 'https://www.youtube.com/*') {
     //        chrome.runtime.sendMessage(links, function (response) {
     //            console.log('response : ' + response);
@@ -6,51 +6,51 @@ function get_title(links) {
     //        });
     //    }
 
-    if (links.includes('search_query')) {
-        console.log('search! ' + links);
-        links = links.replace('/results?search_query=', '');
-        var title = decodeURI(links);
-        if (title.includes('%23')) {
-            title = title.replace('%23', '#');
-        } else {
-            title = '検索：' + title;
-        };
+    if (URL.includes('search_query')) {
+        URL = URL.replace('/results?search_query=', '');
+        var title = decodeURI(URL);
+        title = '検索：' + title;
         return title;
     };
 
-    if (links.includes('twitter.com')) {
-        var id = links.replace('https://twitter.com/', '@');
-        return id;
+    if (URL.includes('twitter.com')) {
+        if (URL.includes('search')) {
+            var tag = URL.replace('https://twitter.com/search?q=%23', '#');
+            return tag;
+        } else {
+            var id = URL.replace('https://twitter.com/', '@');
+            return id;
+        }
     };
 };
 
 function AddLinkButton(DOM) {
     $(DOM).each(function () {
         $(this).addClass("link_button");
-        if ($(this).children('a').attr('href').includes('twitter.com') || $(this).children('a').attr('href').includes('search_query')) {
+        if ($(this).children('a').attr('href').includes('twitter.com')) {
             $(this).children('a').addClass('twitter');
         } else {
             $(this).children('a').addClass('others');
         };
-        console.log('end');
     })
 };
 
 $(function () {
     var links;
     var title;
-    var place_URL = "#description > yt-formatted-string > a";
+    var place_aTag = "#description > yt-formatted-string > a";
     setTimeout(function () {
-        console.log('start');
-        $(place_URL).each(function () {
-            console.log(this);
+        $(place_aTag).each(function () {
             if ($(this).attr('href').includes('twitter.com')) {
                 links = $(this).text();
+            } else if ($(this).attr('href').includes('search_query')) {
+                links = $(this).attr('href').replace('/results?search_query=', '');
+                links = decodeURI(links);
+                links = 'https://twitter.com/search?q=' + links;
             } else {
                 links = $(this).attr('href');
             };
             title = get_title(links);
-            console.log(title);
             $("#info").before("<extension><a href=" + links + ">" + title + "</a></extension>");
         });
         AddLinkButton("extension");
