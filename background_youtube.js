216f8faf3fb34f71c80f9bcfa0c65f3c29f6c02c
clank3140 <clank3140.mo.de@gmail.com>
2019-08-22 01:22:58 +0900

    Youtube動画のタイトルが出るように

--- background_youtube.js
+++ background_youtube.js
@@ -1,17 +1,19 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        var title;
-        $(function () {
-            console.log('ajax');
-            $.ajax({
-                type: 'GET',
-                url: request,
-                success: function (data) {
-                    title = data.match(/<title>(.*)<\/title>/);
-                }
-            })
-            console.log('request : ' + request);
-            console.log(title);
-        });
-        sendResponse(title);
+        var api = 'https://www.googleapis.com/youtube/v3/videos?part=snippet';
+        var token = 'AIzaSyBtJqNli8wKJQE7Ozw_2lUgNlLtYQYlRNM';
+        var videoId = request.replace('https://www.youtube.com/watch?v=', '');
+        api += '&key=' + token + '&id=' + videoId + '&fields=items(snippet(title))';
+        console.log('API : ' + api);
+        var title = new XMLHttpRequest();
+        title.open('GET', api);
+        title.responseType = 'json';
+        title.send();
+        title.onload = function () {
+            console.log(title.response);
+            var videoTitle = title.response.items[0].snippet.title;
+            console.log('send : ' + videoTitle);
+            sendResponse(videoTitle);
+        };
+        return true;
     });
--- content_script.js
+++ content_script.js
@@ -1,34 +1,38 @@
 function get_title(URL) {
-    //    if (links == 'https://www.youtube.com/*') {
-    //        chrome.runtime.sendMessage(links, function (response) {
-    //            console.log('response : ' + response);
-    //            return response;
-    //        });
-    //    }
-
-    if (URL.includes('search_query')) {
+    var defer = $.Deferred();
+    if (URL.includes('/watch')) {
+        URL = 'https://www.youtube.com' + URL;
+        chrome.runtime.sendMessage(URL, function (response) {
+            console.log('response : ' + response);
+            defer.resolve([URL,response]);
+        });
+    } else if (URL.includes('search_query')) {
         URL = URL.replace('/results?search_query=', '');
         var title = decodeURI(URL);
         title = '検索：' + title;
-        return title;
-    };
-
-    if (URL.includes('twitter.com')) {
+        defer.resolve([URL,title]);
+    } else if (URL.includes('https://twitter.com')) {
         if (URL.includes('search')) {
             var tag = URL.replace('https://twitter.com/search?q=%23', '#');
-            return tag;
+            defer.resolve([URL,tag]);
         } else {
             var id = URL.replace('https://twitter.com/', '@');
-            return id;
+            defer.resolve([URL,id]);
         }
-    };
+    } else {
+        defer.resolve([URL,'others']);
+    }
+    return defer.promise(this);
 };
 
 function AddLinkButton(DOM) {
     $(DOM).each(function () {
         $(this).addClass("link_button");
-        if ($(this).children('a').attr('href').includes('twitter.com')) {
+        $(this).children('a').attr('target', '_blank');
+        if ($(this).children('a').attr('href').includes('https://twitter.com')) {
             $(this).children('a').addClass('twitter');
+        } else if ($(this).children('a').attr('href').includes('/watch')) {
+            $(this).children('a').addClass('youtube');
         } else {
             $(this).children('a').addClass('others');
         };
@@ -37,11 +41,10 @@ function AddLinkButton(DOM) {
 
 $(function () {
     var links;
-    var title;
     var place_aTag = "#description > yt-formatted-string > a";
     setTimeout(function () {
         $(place_aTag).each(function () {
-            if ($(this).attr('href').includes('twitter.com')) {
+            if ($(this).attr('href').includes('https%3A%2F%2Ftwitter.com')) {
                 links = $(this).text();
             } else if ($(this).attr('href').includes('search_query')) {
                 links = $(this).attr('href').replace('/results?search_query=', '');
@@ -50,9 +53,11 @@ $(function () {
             } else {
                 links = $(this).attr('href');
             };
-            title = get_title(links);
-            $("#info").before("<extension><a href=" + links + ">" + title + "</a></extension>");
+            $.when(get_title(links)).done(function (URL_title) {
+                console.log('links:' + URL_title[0] + 'title' + URL_title[1]);
+                $("#info").before("<extension><a href=" + URL_title[0] + ">" + URL_title[1] + "</a></extension>");
+                AddLinkButton("extension");
+            });
         });
-        AddLinkButton("extension");
-    }, 2000)
+    }, 2000);
 });
