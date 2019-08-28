9b6fbfe05ec2fd4ca4762c203865d7cc78cdf945
clank3140 <clank3140.mo.de@gmail.com>
2019-08-29 00:48:37 +0900

    [pixiv]「すべて見る」ボタンをイラストの下端に

--- content_script_pixiv.js
+++ content_script_pixiv.js
@@ -1,6 +1,7 @@
 $(function () {
     setTimeout(function () {
         $('span:contains("いいね")').parent().parent().parent().addClass('toRight');
+        $('div:contains("すべて見る")').parent().addClass('subete');
         $('aside > section > h2').after($('.toRight'));
     }, 2000);
 });
