f4df2cc10b89ad907744c2026fc82d02fdd4d862
clank3140 <clank3140.mo.de@gmail.com>
2019-08-28 23:10:56 +0900

    [pixiv]Classが変わるようなので応急処置

--- content_script_pixiv.js
+++ content_script_pixiv.js
@@ -1,6 +1,6 @@
 $(function () {
     setTimeout(function () {
-        console.log($('.sc-LzNOl'));
-        $('.sc-LzNOl').insertAfter('.sc-LzNaW');
+        console.log($('.jkOmkU'));
+        $('.jkOmkU').insertAfter('.jkOmwk');
     }, 2000);
 });
